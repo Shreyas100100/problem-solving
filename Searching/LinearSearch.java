bf2ba3d652cffa8543c802766e59610d026b27b1
Shreyas Kulkarni <ssk.11772299@gmail.com>
2024-11-13 14:45:38 +0530

    LinearSearch

--- Searching/LinearSearch.java
+++ Searching/LinearSearch.java
@@ -1,23 +1,19 @@
 package Searching;
 
-public class LinearSearch {
-    public static void main(String[] args) {
-        int [] nums = {1,4,2,3,8,5,6,9};
-        int target =9;
-        Solution solution = new Solution();
-        System.out.println(solution.LinearSearch(nums,target));
-    }
-}
 
-class Solution
-{
-    public int LinearSearch(int []nums, int target)
+public class LinearSearch {
+    public static int LinearSearch(int []nums, int target)
     {
         for(int i=0;i<nums.length;i++)
         {
             if(nums[i]==target)return i;
         }
         return -1;
+    }
+    public static void main(String[] args) {
+        int [] nums = {1,4,2,3,8,5,6,9};
+        int target =9;
+        System.out.println(LinearSearch(nums,target));
     }
 }
 
